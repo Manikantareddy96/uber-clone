c128609ab6624359cfe2d85f746c7a2fe8557050
Manikantareddy96 <167765029+Manikantareddy96@users.noreply.github.com>
2024-09-25 12:34:59 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "www.manikanta.com"  # Replace with your actual S3 bucket name
+    bucket = "www.manikanta.com.in"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
